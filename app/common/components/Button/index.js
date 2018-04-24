02e84312221bb0ee791bc0c4db553705fda5e1a9
GruFFix <kirill.lomanov@wearevolt.com>
2018-04-24 15:29:18 +0300

    Module #1

---
+++ app/common/components/Button/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import classnames from 'classnames';
+import withStyles from 'withStyles';
+import { compose } from 'recompose';
+import styles from './styles.scss';
+
+const Button = ({ block, to, red, ...rest }) => (
+  React.createElement(
+    to ? Link : 'button',
+    {
+      className: classnames(
+        styles.root,
+        block && styles.isBlock,
+        red && styles.red
+      ),
+      to,
+      ...rest,
+    }
+  )
+);
+
+Button.propTypes = {
+  block: PropTypes.bool,
+};
+
+export default compose(
+  withStyles(styles)
+)(Button);
